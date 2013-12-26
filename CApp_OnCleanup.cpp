652030202863033a7656a2eede58c742b81a16ad
Malaxiz <malaxiz.main@gmail.com>
2013-12-26 22:55:13 +0100

    Update CApp_OnCleanup.cpp

--- CApp_OnCleanup.cpp
+++ CApp_OnCleanup.cpp
@@ -4,7 +4,7 @@ void CApp::OnCleanup()
 {
     for(std::vector<CSnake*>::iterator it = CSnake::SnakeList.begin(); it != CSnake::SnakeList.end(); it++)
     {
-        delete (*it);
+        delete (*it); // This will call the deconstructor of the snake object that is deleted.
     }
 
     CSnake::SnakeList.clear();
