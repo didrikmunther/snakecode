be2e81dd07cbb383b155db57afeb147ee0d3d960
Malaxiz <malaxiz.main@gmail.com>
2013-12-26 20:25:57 +0100

    Update CApp_OnCleanup.cpp

--- CApp_OnCleanup.cpp
+++ CApp_OnCleanup.cpp
@@ -2,23 +2,6 @@
 
 void CApp::OnCleanup()
 {
-//    std::vector<CBlock*>::iterator it;
-//
-//    for ( it = Player->BlockList.begin(); it != Player->BlockList.end(); )
-//    {
-//      delete * it;
-//      it = Player->BlockList.erase(it);
-//    }
-//
-//    for(int i = 0; i < CSnake::SnakeList.size(); i++)
-//    {
-//        if(!CSnake::SnakeList[i]) continue;
-//
-//        //CSnake::SnakeList[i]->BlockList.erase(); // Not working... needs some kind of vector iterator argument
-//
-//        CSnake::SnakeList[i]->BlockList.clear();
-//    }
-
     for(std::vector<CSnake*>::iterator it = CSnake::SnakeList.begin(); it != CSnake::SnakeList.end(); it++)
     {
         delete (*it);
